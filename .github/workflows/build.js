9a132328150720cde692e634c2f1954d971a652d
Valentin Podkamennyi <vpodk@users.noreply.github.com>
2025-02-16 22:25:38 -0800

    cleanup

--- .github/workflows/build.js
+++ .github/workflows/build.js
@@ -10,13 +10,8 @@ const SHEET_ID = "1CnL6zDwlIFSFTqQP7klmcJR7YRFuR2yUPwwp9uuvuzk"; // Public URL
 const FILE_URL = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/export?format=csv`;
 const TEMPLATE = ".github/workflows/template.js";
 
-const build = (data) => {
-  const cwd = path.resolve("./");
-  const template = fs.readFileSync(path.join(cwd, TEMPLATE), "utf8");
-  const dataPath = path.join(cwd, "data");
-  fs.existsSync(dataPath) || fs.mkdirSync(dataPath);
-
-  const map = data.slice(1).reduce((acc, row) => {
+const reduce = (data) =>
+  data.reduce((acc, row) => {
     // Row: [ 0:siteId, 1:bannerURL, 2:targetURL, 3:bannerType, 4:status, 5:priority, 6:rotationDelay ]
     const siteId = row[0];
     if (siteId) {
@@ -26,9 +21,11 @@ const build = (data) => {
 
       const isActive =
         status !== "" &&
+        status !== "0" &&
         status !== "off" &&
-        status !== "false" &&
-        status !== "0";
+        status !== "none" &&
+        status !== "null" &&
+        status !== "false";
 
       if (isActive) {
         acc[siteId] ??= {};
@@ -40,7 +37,14 @@ const build = (data) => {
     return acc;
   }, {});
 
-  for (const [siteId, sizes] of Object.entries(map)) {
+const build = (data) => {
+  const cwd = path.resolve("./");
+  const template = fs.readFileSync(path.join(cwd, TEMPLATE), "utf8");
+  const dataPath = path.join(cwd, "data");
+  const registry = reduce(data.slice(1));
+  fs.existsSync(dataPath) || fs.mkdirSync(dataPath);
+
+  for (const [siteId, sizes] of Object.entries(registry)) {
     for (const [sizeId, banners] of Object.entries(sizes)) {
       const fileName = `${siteId}-${sizeId}.js`;
       const content = template
